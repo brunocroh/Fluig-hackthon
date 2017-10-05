083f337eba3a052b7a2b2c0b19ffdfa50d67ba7f
Daniel Melo <danielmelogpi@gmail.com>
2017-10-04 23:03:20 -0300

    adicionadas rotas e exemplo de layout from f7

--- src/main.js
+++ src/main.js
@@ -1,15 +1,50 @@
 // The Vue build version to load with the `import` command
 // (runtime-only or standalone) has been set in webpack.base.conf with an alias.
 import Vue from 'vue'
+
+// Import F7
+/* eslint-disable no-unused-vars */
+import Framework7 from 'framework7'
+
+// Import F7 Vue Plugin
+import Framework7Vue from 'framework7-vue'
+
+// Import F7 iOS Theme Styles
+/* eslint-disable no-unused-vars */
+// /home/daniel/development/projects/hackathon-fluig/app/node_modules/framework7/dist/css/framework7.md.min.css
+import Framework7Theme from 'framework7/dist/css/framework7.material.css'
+import Framework7ThemeColors from 'framework7/dist/css/framework7.material.colors.min.css'
+// import Framework7ThemeColors from 'framework7/dist/css/framework7.md.'
+// /* OR for Material Theme:
+// import Framework7Theme from 'framework7/dist/css/framework7.md.min.css'
+// import Framework7ThemeColors from 'framework7/dist/css/framework7.md.colors.min.css'
+// */
+
+// Import App Custom Styles
+// import AppStyles from './css/app.css'
+
+// Import Routes
+import Routes from './routes.js'
+
 import App from './App'
-import router from './router'
 
-Vue.config.productionTip = false
-
+// Init F7 Vue Plugin
+Vue.use(Framework7Vue)
+console.log("loaded")
 /* eslint-disable no-new */
+// Init App
 new Vue({
   el: '#app',
-  router,
-  template: '<App/>',
-  components: { App }
+  template: '<app/>',
+  // Init Framework7 by passing parameters here
+  framework7: {
+    root: '#app',
+    /* Uncomment to enable Material theme: */
+    // material: true,
+    routes: Routes
+  },
+  // Register App Component
+  components: {
+    app: App
+  }
 })
